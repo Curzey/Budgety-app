adb502643a76f7f55fdc33357432b9ef62aea030
Carsten <carstena1994@gmail.com>
2019-06-30 14:22:55 +0200

    Added data structure, and addItem functionality

--- app.js
+++ app.js
@@ -1,17 +1,66 @@
 
 /*
-** Architecture: Module pattern. 
-* Budget Controller: Handles the inputtet data.
-* UI Controller: Handles the view part of the data
-* Controller: Connects the above, and handles the app in general.
+**# Architecture: Module pattern. 
+*** Budget Controller: Contains the data structure
+*** UI Controller: Gets and sets data
+*** Controller: Connects the above, and handles the app in general.
 */
 
 
 
 var budgetController = (function() {
 
+    // Expense constructor
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    // Income constructor
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    // Overall data
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // Create new ID
+            // if data has any items of the type, set id to be length + 1, or else set id to 0.
+            ID = data.allItems[type].length > 0 
+                ? data.allItems[type][data.allItems[type].length - 1].id + 1 
+                : 0;
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);                
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push new item into data structure
+            data.allItems[type].push(newItem);
+
+            // Return new element
+            return newItem;
+        }
+    };
+    
 })();
 
 
@@ -21,7 +70,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     };
 
     return {
@@ -31,6 +82,28 @@ var UIController = (function() {
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value  
             } 
+        },
+
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+
+            // Create HTML string with placeholder text
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';                
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            // Replace the placeholder text with actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
         },
 
         getDOMstrings: function() {
@@ -59,15 +132,20 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 
     var ctrlAddItem = function() {
+
         // 1. Get the field input data
         var input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
         // 3. Add the item to the UI
+        UICtrl.addListItem(newItem, input.type);
+
         // 4. Calculate the budget
+
         // 5. Display the budget in the UI
 
-        console.log(input);
     };
 
     return {
